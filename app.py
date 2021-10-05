432caaeb2724c54bec978a387af6ed4a24a31c6e
root <root@PC-Pierre.localdomain>
2021-10-05 15:03:01 +0200

    request json ok

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import *
 from geopy.geocoders import Nominatim
+import requests
+import json
 
 
-
-
+API_URL = "https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&facet=region"
 
 app=Flask(__name__)
 
@@ -20,7 +21,18 @@ def index():
 
 	return render_template('index.html',depart=depart)
 
+@app.route('/api')
+def api():
+	response = requests.get(API_URL)
+	content = json.loads(response.content.decode("utf-8"))
+
+	return content
 
 if __name__ == '__main__':
 
 	app.run(debug=True)
+	'''
+	response = requests.get(API_URL)
+	content = json.loads(response.content.decode("utf-8"))
+	print(content)
+	'''
